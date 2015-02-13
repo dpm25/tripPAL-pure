5ff8582ce157c8d5ba9942727338e7a9d1b3b3b3
rtg8 <rtg8@pitt.edu>
2015-02-13 16:35:53 -0500

    Rudimentary Form
    Added a simple form for submitting trips

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import os
 import sys
@@ -28,7 +12,13 @@ def render_template(handler, templatename, templatevalues):
 class MainPage(webapp2.RequestHandler):
     def get(self):
         render_template(self, 'index.html', {})
+		
+class MakePost(webapp2.RequestHandler):
+	def get(self):
+		render_template(self, 'post.html', {})
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', MainPage),
+	('/makepost', MakePost),
+	
 ], debug=True)
