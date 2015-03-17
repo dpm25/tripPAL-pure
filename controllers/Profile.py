d890f7333558064989a531e625543a0b5c8e6079
dpm25 <danielmahoney6@gmail.com>
2015-03-16 21:46:11 -0400

    Working profile with djengo stuffs

--- controllers/Profile.py
+++ controllers/Profile.py
@@ -19,17 +19,36 @@ class Profile(webapp2.RequestHandler):
 	def post(self):
 		# get current user
 		user = users.get_current_user()
+		nName = user.nickname()
+		
+		q = UserInfo.query(UserInfo.nickname == nName).fetch()
 
-		# populate Profile stored object
-		profile = UserInfo()
-		profile.userName = user.user_id()
-		profile.nickname = user.nickname()
-		profile.fname = self.request.get('fname')
-		profile.lname = self.request.get('lname')
-		profile.phone = self.request.get('phone')
-		profile.address = self.request.get('address')
-		profile.city = self.request.get('city')
-		profile.state = self.request.get('zip')
-		profile.time = int(time.time())
-		profile.put()
+		if q:
+			# populate existing Profile stored object
+			profile = q[0]
+			profile.userName = user.user_id()
+			profile.nickname = user.nickname()
+			profile.firstName = self.request.get('fname')
+			profile.lastName = self.request.get('lname')
+			profile.phone = self.request.get('phone')
+			profile.address = self.request.get('address')
+			profile.city = self.request.get('city')
+			profile.state = self.request.get('state')
+			profile.zip = self.request.get('zip')
+			profile.time = int(time.time())
+			profile.put()
+		else:
+			profile = UserInfo()
+			profile.userName = user.user_id()
+			profile.nickname = user.nickname()
+			profile.firstName = self.request.get('fname')
+			profile.lastName = self.request.get('lname')
+			profile.phone = self.request.get('phone')
+			profile.address = self.request.get('address')
+			profile.city = self.request.get('city')
+			profile.state = self.request.get('state')
+			profile.zip = self.request.get('zip')
+			profile.time = int(time.time())
+			profile.put()
+		
 		render_template(self, 'index.html', {})
--- controllers/viewProfile.py
+++ controllers/viewProfile.py
@@ -4,6 +4,10 @@ import sys
 
 from google.appengine.ext.webapp import template
 from google.appengine.ext import ndb
+from google.appengine.api import users
+
+from model.User import *
+
 
 def render_template(handler, templatename, templatevalues):
 	path = os.path.join(os.path.dirname(__file__), "../templates/", templatename)
@@ -13,4 +17,26 @@ def render_template(handler, templatename, templatevalues):
 
 class ViewProfile(webapp2.RequestHandler):
 	def get(self):
-		render_template(self, 'profile.html', {})
+		user = users.get_current_user()
+		nName = user.nickname()
+
+		q = UserInfo.query(UserInfo.nickname == nName).fetch()
+
+		firstName = q[0].firstName
+		lastName = q[0].lastName
+		phone = q[0].phone
+		address = q[0].address
+		city = q[0].city
+		state = q[0].state
+		zip = q[0].zip
+
+		params = {
+			'firstname': firstName,
+			'lastname': lastName,
+			'phone': phone,
+			'address': address,
+			'city': city,
+			'state': state,
+			'zip': zip	
+		}
+		render_template(self, 'profile.html', params)
