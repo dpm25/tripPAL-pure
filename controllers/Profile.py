f62e6d9765318896f53ac976f04be152103c672a
dpm25 <danielmahoney6@gmail.com>
2015-04-11 14:09:37 -0400

    form validation

--- controllers/Profile.py
+++ controllers/Profile.py
@@ -20,35 +20,71 @@ class Profile(webapp2.RequestHandler):
 		# get current user
 		user = users.get_current_user()
 		nName = user.nickname()
-		
-		q = UserInfo.query(UserInfo.nickname == nName).fetch()
 
-		if q:
-			# populate existing Profile stored object
-			profile = q[0]
-			profile.userName = user.user_id()
-			profile.nickname = user.nickname()
-			profile.firstName = self.request.get('fname')
-			profile.lastName = self.request.get('lname')
-			profile.phone = self.request.get('phone')
-			profile.address = self.request.get('address')
-			profile.city = self.request.get('city')
-			profile.state = self.request.get('state')
-			profile.zip = self.request.get('zip')
-			profile.time = int(time.time())
-			profile.put()
+		# getting all the form values
+		fname = self.request.get('fname')
+		lname = self.request.get('lname')
+		phone = self.request.get('phone')
+		address = self.request.get('address')
+		city = self.request.get('city')
+		state = self.request.get('state')
+		zip = self.request.get('zip')
+
+		# errors is going to contain list of errors from the form
+		errors=[]
+
+		# checking if the first and last name exist
+		if fname is None or fname=="":
+			errors+=["First Name is missing"]
+		if lname is None or fname=="":
+			errors+=['last name is missing']
+		# checking the length of the phone number
+		if len(phone)<10 or len(phone)>11:
+			errors+=["invalid phone number"]
+		# checking is the address, city, state, and zip exists
+		if address is None or address=="":
+			errors+=["address is missing"]
+		if city is None or city=="":
+			errors+=["city is missing"]
+		if state is None or state=="":
+			errors+=["invalid or missing state"]
+		if len(zip)!=5:
+			errors+=["invalid or missing zip"]
+
+		if len(errors)>0:
+			for error in errors:
+				render_template(self, 'contactUs.html', {})
+		# if no errors in form
 		else:
-			profile = UserInfo()
-			profile.userName = user.user_id()
-			profile.nickname = user.nickname()
-			profile.firstName = self.request.get('fname')
-			profile.lastName = self.request.get('lname')
-			profile.phone = self.request.get('phone')
-			profile.address = self.request.get('address')
-			profile.city = self.request.get('city')
-			profile.state = self.request.get('state')
-			profile.zip = self.request.get('zip')
-			profile.time = int(time.time())
-			profile.put()
+			q = UserInfo.query(UserInfo.nickname == nName).fetch()
+
+			if q:
+				# populate existing Profile stored object
+				profile = q[0]
+				profile.userName = user.user_id()
+				profile.nickname = user.nickname()
+				profile.firstName = self.request.get('fname')
+				profile.lastName = self.request.get('lname')
+				profile.phone = self.request.get('phone')
+				profile.address = self.request.get('address')
+				profile.city = self.request.get('city')
+				profile.state = self.request.get('state')
+				profile.zip = self.request.get('zip')
+				profile.time = int(time.time())
+				profile.put()
+			else:
+				profile = UserInfo()
+				profile.userName = user.user_id()
+				profile.nickname = user.nickname()
+				profile.firstName = self.request.get('fname')
+				profile.lastName = self.request.get('lname')
+				profile.phone = self.request.get('phone')
+				profile.address = self.request.get('address')
+				profile.city = self.request.get('city')
+				profile.state = self.request.get('state')
+				profile.zip = self.request.get('zip')
+				profile.time = int(time.time())
+				profile.put()
+			
+			render_template(self, 'index.html', {})		
 		
-		render_template(self, 'index.html', {})
